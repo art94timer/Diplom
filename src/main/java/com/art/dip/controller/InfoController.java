dcd1273fca4fdec691d7edbd3d70ec915045f164
art94timer <timerbaev94@bk.ru>
2021-02-03 17:13:11 +0300

    add FlyWay. Tests. Error page. AccessDeniedHandler. New feature OnCloseFaculty. Prepare for show

--- src/main/java/com/art/dip/controller/InfoController.java
+++ src/main/java/com/art/dip/controller/InfoController.java
@@ -2,6 +2,7 @@ package com.art.dip.controller;
 
 import com.art.dip.service.interfaces.InfoService;
 import com.art.dip.utility.dto.AccountInfoDTO;
+import com.art.dip.utility.dto.FacultyInfoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.TimeZone;
 
 
@@ -50,9 +55,18 @@ public class InfoController {
 
 	@GetMapping("/faculty/{id}")
 	public String getFacultyInfo(@PathVariable("id") Integer facultyId, Model model, HttpServletRequest request, TimeZone timeZone) {
+		FacultyInfoDTO facultyInfo = service.getFacultyInfo(facultyId);
+		LocalDateTime updateTime = facultyInfo.getUpdateTime();
+		ZonedDateTime zonedDateTime = updateTime.atZone(ZoneId.of("UTC"));
+		LocalDateTime time = zonedDateTime.withZoneSameInstant(LocaleContextHolder.getTimeZone().toZoneId()).toLocalDateTime();
+		facultyInfo.setUpdateTime(time);
+
+
+
 		model.addAttribute("faculty",service.getFacultyInfo(facultyId));
 		request.getCookies();
-		model.addAttribute("zone",LocaleContextHolder.getTimeZone());
+
+		model.addAttribute("zone","");
 		return "facultyInfo";
 
 	}
